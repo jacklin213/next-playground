88acb735ed07c3259dfeda1796c32b2e650c9ab5
Jack Lin <jacklin213@gmail.com>
2023-09-09 06:28:54 +0000

    Remove next.config.js options which are injected by Github actions

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /**
-   * Set base path. This is usually the slug of your repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: "/next-playground",
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
-   */
-  images: {
-    unoptimized: true
-  },
+  // actions/configure-pages@v3 automatically injects 'basePath' and 'image.unoptimized' keys
   /**
    * Enable static exports for the App Router.
    *
