3c15d6e35ba948dd283971b89d7fb49a0d9314a8
Jack Lin <jacklin213@gmail.com>
2023-09-09 06:50:32 +0000

    Attempt to fix image loading issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = undefined;
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  console.log(`GitHub actions running. Setting assetPrefix to ${assetPrefix}`);
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // actions/configure-pages@v3 automatically injects 'basePath' and 'image.unoptimized' keys
+  /**
+   * Ensure assets are correctly loaded from repository when in Github Pages
+   */
+  assetPrefix,
   /**
    * Enable static exports for the App Router.
    *
